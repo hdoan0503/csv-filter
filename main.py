3f28d234426f602b7d8ba5154d69f0ed11789fc1
hieudoan9835 <hmd53@drexel.edu>
2022-05-23 12:29:22 -0400

    Add comments

--- main.py
+++ main.py
@@ -9,9 +9,10 @@
 """
 import csv
 
+#This function will read the provided csv and parse into a easy to work with data structure which is list
 def csv_parser(filename):
     records = []
-    with open(filename, 'r', encoding='utf-8-sig') as csv_file:
+    with open(filename, 'r', encoding='utf-8-sig') as csv_file: #open and read csv files
         reader = csv.reader(csv_file)
         header = next(reader)
         
@@ -20,25 +21,30 @@ def csv_parser(filename):
     
     return header, records
 
-
+#This function will take the records, ask for user input and start to filter out the result
 def filterRequest(records):
     result = []
     
     userInput = input("Enter first name, last name or birth year: ")
+    
+    #Check if user input is birth year?
     try:
         int(userInput)
-        isDob = True
+        isBirthYear = True
     except ValueError:
-        isDob = False
+        isBirthYear = False
 
-    if isDob:
+    #If it's the birth year, filter out the dob that match the birth year from user input 
+    if isBirthYear:
         for i in records:
             if(userInput == i[2][0:4]):
-                result.append(i)
+                result.append(i) #if match, store the record into a result
+    
+    #If not a birth year, filter out the first name and last name that match user input
     else:
         for i in records:
             if(userInput.capitalize() == i[0] or userInput.capitalize() == i[1]):
-                result.append(i)
+                result.append(i) #if match, store the record into a result
     return result
 
 
@@ -46,7 +52,7 @@ def filterRequest(records):
 if __name__ == "__main__":
     header, records = csv_parser('example.csv')
     results = filterRequest(records)
-
+    ##print out the result
     print("------------Results--------------")
     print(",".join(header))
     for i in results:
